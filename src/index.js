3d2e4d31ebbd757822ebdedfc2db2bdf32f9cc33
Junhwan Im <junhwanim@hotmail.com>
2021-05-04 12:24:02 -0400

    add App.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from 'react-dom'
+import App from "./App"
 
 const rootDiv = document.getElementById("root")
 
-ReactDOM.render(<div>Hello</div>, rootDiv)
+ReactDOM.render(<App/>, rootDiv)
